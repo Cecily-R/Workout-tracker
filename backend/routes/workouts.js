a68376870e4876627c1e9164bf2529b1493a827b
Cecily Rowland <cec1rowland@gmail.com>
2023-01-10 16:55:30 +0000

    Created workout model and added to POST request

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -6,23 +7,29 @@ router.get('/', (req, res) => {
   res.json({mssg: 'GET all workouts'})
 })
 
-router.get('/:id', (res, req) => {
+router.get('/:id', (req, res) => {
   res.json({mssg: 'GET a single workout'})
 })
 
-router.post('/', (req, res) => {
-  res.json({mssg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+  const {title, reps, weight} = req.body
+
+  try {
+    const workout = await Workout.create({title, reps, weight}) 
+    res.status(200).json(workout)
+  } catch(error) {
+    res.status(400).json({error: error.message})
+
+  }
 })
 
 router.delete('/:id', (req, res) => {
   res.json({mssg: 'DELETE a workout'})
 })
 
-router.patch('/:id', (res, req) => {
+router.patch('/:id', (req, res) => {
   res.json({mssg: 'UPDATE a single workout'})
 })
-
-
 
 module.exports = router
 
