d7837ecac9ab6710544946501427466fe1d61e5b
Cecily Rowland <cec1rowland@gmail.com>
2023-01-11 13:26:58 +0000

    Created all controller functions and implemented in routes

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,15 +1,20 @@
 const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
 
 // Get all workouts
 const getWorkouts = async (req, res) => {
   const workouts = await Workout.find({}).sort({createdAt: -1})
 
-  res.status(200).son(workouts)
+  res.status(200).json(workouts)
 }
 
 // Get single workout
 const getWorkout = async (req, res) => {
   const { id } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
 
   const workout = await Workout.findById(id)
 
@@ -34,6 +39,47 @@ const createWorkout = async (req, res) => {
 
 // Delete a workout 
 
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findOneAndDelete({_id: id})
+
+  if (!workout) {
+    return res.status(400).json({error: 'No such workout!'})
+  }
+
+  res.status(200).json(workout)
+
+}
+
+// Update workout
+const updateWorkout = async (req, res) => {
+    const { id } = req.params
+  
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+      ...req.body
+    })
+    
+    if (!workout) {
+      return res.status(400).json({error: 'No such workout!'})
+    }
+    res.status(200).json(workout)
+
+}
+
+
 module.exports = {
-  createWorkout
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,25 @@
 const express = require('express')
 const {
   createWorkout, 
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout
+
 } = require('../controllers/workoutController')
 
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
-router.get('/:id', (req, res) => {
-  res.json({mssg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
 router.post('/', createWorkout)
 
-router.delete('/:id', (req, res) => {
-  res.json({mssg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
-router.patch('/:id', (req, res) => {
-  res.json({mssg: 'UPDATE a single workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
 
